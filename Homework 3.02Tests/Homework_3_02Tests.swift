fb1b6e0ad0bac2970f1ff7321c2d657c4b634288
Daniil Maiboroda <Dant2603@gmail.com>
2024-10-30 19:50:33 +0300

    Initial Commit

---
+++ Homework 3.02Tests/Homework_3_02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3_02Tests.swift
+//  Homework 3.02Tests
+//
+//  Created by Мария Гетманская on 30.10.2024.
+//
+
+import Testing
+@testable import Homework_3_02
+
+struct Homework_3_02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
